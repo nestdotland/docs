e2814e325c31d16a4d952aaa28b99e32c67e3526
Marton Lederer <martondeveloper@gmail.com>
2020-07-11 08:51:08 +0200

    Updated footer names

--- docs/.vuepress/theme/components/Footer.vue
+++ docs/.vuepress/theme/components/Footer.vue
@@ -10,7 +10,7 @@
 
         <div class="authors">
 
-          <p> A platform by <a href="https://github.com/tbaumer22"><b>tbaumer22</b></a>, <a href="https://github.com/zorbyte"><b>zorbyte</b></a>, <a href="https://github.com/justablob"><b>blob</b></a>, &amp; <a href="https://github.com/nestdotland/nest.land#contributors"><b>friends</b></a>.</p>
+          <p> A platform by <a href="https://github.com/t8"><b>t8</b></a>, <a href="https://github.com/zorbyte"><b>zorbyte</b></a>, <a href="https://github.com/justablob"><b>blob</b></a>, &amp; <a href="https://github.com/nestdotland/nest.land#contributors"><b>friends</b></a>.</p>
           <p>Made with <svg data-v-27c0ec39="" aria-hidden="true" focusable="false" data-prefix="fa" data-icon="heart" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="svg-inline--fa fa-heart fa-w-16"><path data-v-27c0ec39="" fill="currentColor" d="M462.3 62.6C407.5 15.9 326 24.3 275.7 76.2L256 96.5l-19.7-20.3C186.1 24.3 104.5 15.9 49.7 62.6c-62.8 53.6-66.1 149.8-9.9 207.9l193.5 199.8c12.5 12.9 32.8 12.9 45.3 0l193.5-199.8c56.3-58.1 53-154.3-9.8-207.9z" class=""></path></svg> for the Deno community.</p>
 
         </div>
